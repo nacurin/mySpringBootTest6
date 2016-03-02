cc1d7dfd41160468ffa6cf9914db55981ad3fe7a
sunyehong <165793@sh.lianjia.com>
2016-03-02 14:43:50 +0800

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,24 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Configuration
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
